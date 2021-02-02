caf67635436bcc371b2511832f7b836ff1e94c67
Gianni Mondello <gianni.mondello@live.it>
2021-02-02 15:35:46 +0100

    fix nextlne in esercizio 1

--- src/Esercizio1/App.java
+++ src/Esercizio1/App.java
@@ -7,20 +7,26 @@ public class App {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
+		App.run();
+		System.out.println("Vuoi fare un'altra operazione?");
+		
+		String resp = scanner.nextLine();
+
+		if (resp.equals("N")) {
+			System.exit(2);
+		} else if (resp.equals("Y")) {
+			App.run();
+		}
+
+	}
+
+	public static void run() {
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Insertisci due numeri");
 
 		int num1 = scanner.nextInt();
 		int num2 = scanner.nextInt();
 
 		System.out.println(num1 + num2);
-		System.out.println("Vuoi fare un'altra operazione?");
-		scanner.nextLine();
-		String resp = scanner.nextLine();
-		System.out.println(resp);
-
-		if (resp == "N") {
-			System.exit(2);
-		}
-
 	}
 }
