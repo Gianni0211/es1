a2228538b1de88404a871a8f1d01139940f77e0b
Gianni Mondello <gianni.mondello@live.it>
2021-02-03 09:36:24 +0100

    applicazione continua, e refactoring

--- src/Esercizio1/App.java
+++ src/Esercizio1/App.java
@@ -5,28 +5,34 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		Scanner scanner = new Scanner(System.in);
-		App.run();
-		System.out.println("Vuoi fare un'altra operazione?");
-		
-		String resp = scanner.nextLine();
 
-		if (resp.equals("N")) {
-			System.exit(2);
-		} else if (resp.equals("Y")) {
-			App.run();
-		}
+		Scanner scanner = new Scanner(System.in);
+		// run(scanner);
+
+		String resp = "S";
+
+		do {
+
+			run(scanner);
+			System.out.println("Vuoi fare un'altra operazione?");
+			scanner.nextLine();
+			resp = scanner.nextLine();
+
+		} while (!resp.equals("N"));
+
+		scanner.close();
 
 	}
 
-	public static void run() {
-		Scanner scanner = new Scanner(System.in);
+	public static void run(Scanner scanner) {
+
 		System.out.println("Insertisci due numeri");
 
 		int num1 = scanner.nextInt();
 		int num2 = scanner.nextInt();
 
 		System.out.println(num1 + num2);
+
 	}
+
 }
